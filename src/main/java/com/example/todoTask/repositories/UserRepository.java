0a696a94e8195a3c5234d67c62fad593b46b0252
unknown <contatogabrielcruzrodrigues@gmail.com>
2023-09-11 09:03:29 -0300

    creating new folder configuration

--- src/main/java/com/example/todoTask/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.todoTask.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.todoTask.models.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
